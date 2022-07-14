60e78fe42a7af9a9ceff1b54a9a655b921722857
mohamed-younes <mohamed.rouabehi.code@gmail.com>
2022-07-14 16:23:33 +0200

    chore: configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,33 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended"
-  ]
-}
+    "root": true,
+    "extends": [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript/recommended"
+    ],
+    "env": {
+        "browser": true,
+        "amd": true,
+        "node": true
+    },
+    "rules": {
+        "indent": ["error", 4],
+        "semi": ["error", "always"],
+        "semi-spacing": "error",
+        "space-before-function-paren": ["error", "always"],
+        "curly": "error",
+        "default-case": "error",
+        "max-lines-per-function": ["error", 25],
+        "max-params": ["error", 4],
+        "no-console": ["warn"],
+        "arrow-spacing": ["error", { "before": true, "after": true }],
+        "arrow-parens": ["error", "always"],
+        "comma-style": ["error", "last"],
+        "object-curly-spacing": ["error", "always"],
+        "no-unneeded-ternary": "error",
+        "prefer-const": "error",
+        "spaced-comment": ["error", "always"]
+    }
+};
