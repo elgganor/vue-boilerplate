ef07debe05ba83efd1f268d1438b485476ff56f8
Elgganor <mohamed.rouabehi.code@gmail.com>
2022-07-14 16:45:09 +0200

    chore: setup env file and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import type { ConfigEnv, UserConfigExport } from "vite";
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export default defineConfig(({ mode }: ConfigEnv): UserConfigExport => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [vue()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url))
+			}
+		},
+		server: {
+			host: env.HOST ?? '0.0.0.0',
+			port: Number(env.PORT) ?? 3000
+		}
+	};
 });
